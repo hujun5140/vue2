1a154b2db9faddf292bd59b3fb9c980305e9052f
雪花红梅 <mvp5140@163.com>
2019-03-28 00:06:34 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,21 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+// 解决移动端点击300毫秒的延迟
+import fastClick from 'fastclick'
+// 解决默认样式的统一
+import './assets/style/reset.css'
+// 解决1px边框的问题
+import './assets/style/border.css'
+
+Vue.config.productionTip = false
+fastClick.attach(document.body)
+
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
