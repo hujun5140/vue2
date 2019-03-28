2ab144aec7416ea532676090f75298cb608c58e1
雪花红梅 <mvp5140@163.com>
2019-03-28 20:00:27 +0800

    'change'

--- src/main.js
+++ src/main.js
@@ -5,14 +5,19 @@ import App from './App'
 import router from './router'
 // 解决移动端点击300毫秒的延迟
 import fastClick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 // 解决默认样式的统一
-import './assets/styles/reset.css'
+import 'styles/reset.css'
 // 解决1px边框的问题
-import './assets/styles/border.css'
-import './assets/styles/iconfont.css'
+import 'styles/border.css'
+import 'styles/iconfont.css'
+import 'swiper/dist/css/swiper.css'
+
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
+
 
 new Vue({
   el: '#app',
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="wrapper">
+        <swiper :options="swiperOption">
+            <swiper-slide v-for="item of swiperList" :key="item.id">
+                <img class="swiper-img" :src="item.imgUrl" />
+            </swiper-slide>
+            <div class="swiper-pagination"  slot="pagination"></div>
+        </swiper>
+    </div>
+</template>
+<script>
+export default {
+    name: 'HomeSwiper',
+    data(){
+        return {
+            swiperOption: {
+                pagination: '.swiper-pagination',
+                loop: true
+            },
+            swiperList:[{
+                id: '0001',
+                imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/18/c1e1a713c0543a02.jpg_750x200_c1295fcf.jpg'
+            },{
+                id: '0002',
+                imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/4c816b1e5e73973ade5a8dac7f808ff1.jpg_750x200_0c475545.jpg'
+            }]
+        }
+    }
+}
+</script>
+<style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet-active
+    background: #fff;
+.wrapper
+    overflow: hidden
+    width:100%
+    height: 0
+    padding-bottom: 26.66%
+    background: #eee
+    .swiper-img
+        width:100%
+</style>
