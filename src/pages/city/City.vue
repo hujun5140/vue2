21d51ee0a50f5e2a9587038bdcb609e98c9d8420
雪花红梅 <mvp5140@163.com>
2019-03-30 18:57:33 +0800

    ajax city

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,11 +2,12 @@
     <div>
         <city-header></city-header>
         <city-search></city-search>
-        <city-list></city-list>
-        <city-alphabet></city-alphabet>
+        <city-list :cities="cities" :hot="hotCities"></city-list>
+        <city-alphabet :cities="cities"></city-alphabet>
     </div>
 </template>
 <script>
+import axios from 'axios'
 import CityHeader from './components/Header'
 import CitySearch from './components/Search'
 import CityList from './components/List'
@@ -18,6 +19,28 @@ export default {
         CitySearch,
         CityList,
         CityAlphabet
+    },
+    data(){
+        return {
+            cities: {},
+            hotCities: []
+        }
+    },
+    methods: {
+        getCityInfo(){
+            axios.get('/api/city.json').then(this.getCityInfoSucc)
+        },
+        getCityInfoSucc(res){
+            console.log(res)
+            res = res.data
+            if(res.ret && res.data){
+                this.cities = res.data.cities
+                this.hotCities = res.data.hotCities
+            }
+        }
+    },
+    mounted(){
+        this.getCityInfo()
     }
 }
 </script>
