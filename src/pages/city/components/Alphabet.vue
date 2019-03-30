899e20acab3b48e7c8a8194e34a6cad116f478b8
雪花红梅 <mvp5140@163.com>
2019-03-30 15:12:19 +0800

    finish city css

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <ul class="list">
+            <li class="item">A</li>
+            <li class="item">A</li>
+            <li class="item">A</li>
+            <li class="item">A</li>
+            <li class="item">A</li>
+            <li class="item">A</li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+    name: 'CityAlphabet'
+}
+</script>
+<style lang="scss" scoped>
+    .list {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        position: absolute;
+        top: 1.58rem;
+        right: 0;
+        bottom: 0;
+        width: .4rem;
+        .item {
+            text-align:  center;
+            line-height: .4rem;
+            color: $bgColor;
+        }
+    }
+</style>
